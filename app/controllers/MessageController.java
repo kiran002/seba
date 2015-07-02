8b55f0ca17a0bcbfb5e0310d2cb5877b112dd5c5
Kiran <austin.kiran@gmail.com>
2015-07-02 15:24:42 +0200

    message controller

--- app/controllers/MessageController.java
+++ app/controllers/MessageController.java
@@ -15,13 +15,14 @@ public class MessageController extends Controller {
 
 	@play.db.jpa.Transactional
 	public Result sendMessage() {
-		if (session("usrId") != null && session("usrId").length() > 0) {			
+		if (session("usrid") != null && session("usrid").length() > 0) {			
 			DynamicForm form = Form.form().bindFromRequest();
 			Messages msg = new Messages();
-			msg.FromUserId = Integer.parseInt(session("usrId"));
+			msg.FromUserId = Integer.parseInt(session("usrid"));
 			msg.ToUserId = Integer.parseInt(form.get("sendTo"));
 			msg.ListingId = Integer.parseInt(form.get("listingId"));
 			msg.Message = form.get("msg");
+			msg.save();
 			return ok(); // successful have to forward
 		}
 		return ok(); // unsuccessful need to validate and other stuff
